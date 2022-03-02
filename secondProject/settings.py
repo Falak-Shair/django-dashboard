edb459af9a67d2d5d4b48c258f91ec19529050b2
Falak Shair <falakshair.563@gmail.com>
2022-03-02 19:35:37 +0500

    changed

--- secondProject/settings.py
+++ secondProject/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     # 'django_filters',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
